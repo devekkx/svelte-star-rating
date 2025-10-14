741b6ffd3ba4865fef3e5675a3234ca827957970
Dev Ekkx <devekkx@gmail.com>
2025-10-14 11:52:39 +0000

    chore: configure project structure

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import {defineConfig} from 'vite'
+import {svelte} from '@sveltejs/vite-plugin-svelte'
+import * as path from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+    plugins: [svelte()],
+    build: {
+        lib: {
+            entry: path.resolve(__dirname, 'src/index.ts'),
+            name: '@dev-ekkx/svelte-star-rating',
+            formats: ['es', 'cjs'],
+            fileName: (format) => `index.${format}.js`
+        },
+        rollupOptions: {
+            external: ['svelte'],
+            output: {
+                globals: {
+                    svelte: 'Svelte'
+                }
+            }
+        }
+    }
 })
