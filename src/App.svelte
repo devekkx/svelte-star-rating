ec1324445d9ae0aae3153f170ce933704b27fbaa
Dev Ekkx <devekkx@gmail.com>
2025-10-14 15:59:49 +0000

    feature: create star rating component

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,25 @@
-<script lang="ts"></script>
+<script lang="ts">
+    import StarRating from "./lib/star-rating.svelte";
+    import type {ConfigI} from "./lib/types";
+
+    const config = $state<ConfigI>({
+        readonly: false,
+        maxVal: 5,
+        minVal: 0,
+        step: 0.1,
+        numOfStars: 5,
+        starConfig: {
+            size: 26,
+            filledColor: '#F98416',
+            unfilledColor: '#5D5D5D',
+        },
+    })
+
+    let value = $state(4.4)
+    $inspect(value)
+
+</script>
 
 <main>
+    <StarRating bind:value {config}/>
 </main>
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -2,17 +2,14 @@ export interface StarConfigI {
     size: number,
     filledColor: string,
     unfilledColor: string
-    filledStrokeColor: string
-    unfilledStrokeColor: string
 }
 
 export interface ConfigI {
     readonly: boolean
     numOfStars: number,
-    value: {
-        minVal: number,
-        maxVal: number,
-        step: number,
-    }
+    minVal: number,
+    maxVal: number,
+    step: number,
+
     starConfig: StarConfigI
 }
